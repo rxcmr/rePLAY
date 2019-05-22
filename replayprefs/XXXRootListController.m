810bff3f8939abbd86ed057af39d8250efb28537
rxcmr <lythe1107@icloud.com>
2019-05-22 15:17:47 +0800

    version bump and remove that comment

--- replayprefs/XXXRootListController.m
+++ replayprefs/XXXRootListController.m
@@ -16,7 +16,7 @@
 	int status;
 	const char* args[] = {"killall", "-9", "Deepworld", NULL};
 	posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
-	waitpid(pid, &status, WEXITED); //wait untill the process completes (only if you need to do that)
+	waitpid(pid, &status, WEXITED); 
 }
 
 @end
