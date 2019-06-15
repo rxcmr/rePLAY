24d053a7a804abeece43a3bbfb73f43a604af09b
rxcmr <lythe1107@icloud.com>
2019-06-15 21:43:06 +0800

    git pull

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-cd /var/mobile/rePLAY; make package install FINALPACKAGE=1; git add .; git commit -m "updating test device"; git push
+cd /var/mobile/rePLAY; git pull; make package install FINALPACKAGE=1; git add .; git commit -m "updating test device"; git push
